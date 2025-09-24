2b2d7688253cc29be32f811622358f67a3d835bb
timera <timerakkle@gmail.com>
2025-09-24 15:50:02 -0400

    Making the default behavior of exporting materials not dependent on layer color

--- glTF-BinExporter/glTFBinExporterPlugin.cs
+++ glTF-BinExporter/glTFBinExporterPlugin.cs
@@ -135,7 +135,7 @@ namespace glTF_BinExporter
         }
 
         private const string useDisplayColorForUnsetMaterialsKey = "UseDisplayColorForUnsetMaterials";
-        public const bool UseDisplayColorForUnsetMaterialsDefault = true;
+        public const bool UseDisplayColorForUnsetMaterialsDefault = false;
 
         public static bool UseDisplayColorForUnsetMaterials
         {
